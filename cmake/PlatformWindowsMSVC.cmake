a0179c9a7c999e76fae7d899204412d37baa9ef5
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2014-05-27 00:03:47 +0200

    fix compilation on windows

--- cmake/PlatformWindowsMSVC.cmake
+++ cmake/PlatformWindowsMSVC.cmake
@@ -22,7 +22,7 @@ set(DEFAULT_COMPILE_DEFS_RELEASE
 
 
 set(WIN32_COMPILE_FLAGS
-    /nologo /Zc:wchar_t /Zc:forScope /GR /Zi /fp:precise /MP /W3 /wd4351 /wd4250
+    "/nologo /Zc:wchar_t /Zc:forScope /GR /Zi /fp:precise /MP /W3 /wd4351 /wd4250"
     # nologo       -> no logo
     # Zc:wchar_t   -> treat wchar_t as built-in type: yes
     # Zc:forScope  -> force conformance in for loop scope: Yes
