96d7b120936d0d8ec954a06e932660b242641e90
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2014-09-28 21:01:17 +0200

    implement modification of poly data centroids with neighbor cell updates

--- core/table_model/QVtkTableModelPolyData.cpp
+++ core/table_model/QVtkTableModelPolyData.cpp
@@ -1,12 +1,16 @@
 #include "QVtkTableModelPolyData.h"
 
 #include <cassert>
+#include <QSet>
 
 #include <vtkPolyData.h>
 #include <vtkCellData.h>
-#include <vtkDataArray.h>
+#include <vtkIdTypeArray.h>
 #include <vtkCell.h>
+#include <vtkIdList.h>
+#include <vtkPolygon.h>
 
+#include <core/vtkhelper.h>
 #include <core/data_objects/DataObject.h>
 
 
@@ -81,12 +85,80 @@ QVariant QVtkTableModelPolyData::headerData(int section, Qt::Orientation orienta
 
 bool QVtkTableModelPolyData::setData(const QModelIndex & index, const QVariant & value, int role)
 {
-    if (role != Qt::EditRole || index.column() != 5 || !m_vtkPolyData)
+    if (role != Qt::EditRole || index.column() < 2 || !m_vtkPolyData)
         return false;
 
-    /** TODO */
+    // get the delta between current and changed centroid coordinate value
+    bool ok;
+    double formerValue = data(index).toDouble(&ok);
+    assert(ok);
+    double newValue = value.toDouble(&ok);
+    if (!ok)
+        return false;
 
-    return false;
+    double valueDelta = value.toDouble(&ok) - formerValue;
+
+
+    vtkIdType cellId = index.row();
+    int component = index.column() - 2;
+    assert(component >= 0 && component < 3);
+
+    vtkCell * cell = m_vtkPolyData->GetCell(cellId);
+    vtkIdList * pointIds = cell->GetPointIds();
+
+
+    // apply the value delta to all point of the triangle
+    double point[3];
+    for (int i = 0; i < pointIds->GetNumberOfIds(); ++i)
+    {
+        vtkIdType pointId = pointIds->GetId(i);
+        m_vtkPolyData->GetPoint(pointId, point);
+        point[component] += valueDelta;
+        m_vtkPolyData->GetPoints()->SetPoint(pointId, point);
+    }
+
+    // apply requested modification to the centroid
+    double centroid[3];
+    vtkDataArray * centroids = m_vtkPolyData->GetCellData()->GetArray("centroid");
+    assert(centroids && centroids->GetNumberOfComponents() == 3);
+    centroids->GetTuple(cellId, centroid);
+    centroid[component] = newValue;
+    centroids->SetTuple(cellId, centroid);
+
+
+    // also adjust the centroids of adjacent triangles
+    VTK_CREATE(vtkIdList, vertex);
+    vertex->SetNumberOfIds(1);
+    VTK_CREATE(vtkIdList, vertexNeighbors);
+
+    QSet<vtkIdType> neighborCellIds;
+
+    // find cell neighbors
+    for (vtkIdType i = 0; i < pointIds->GetNumberOfIds(); ++i)
+    {
+        vertex->SetId(0, pointIds->GetId(i));
+        m_vtkPolyData->GetCellNeighbors(cellId, vertex, vertexNeighbors);
+
+        for (vtkIdType i = 0; i < vertexNeighbors->GetNumberOfIds(); ++i)
+            neighborCellIds << vertexNeighbors->GetId(i);
+    }
+
+
+    VTK_CREATE(vtkIdTypeArray, idArray);
+    for (vtkIdType neighborCellId : neighborCellIds)
+    {
+        vtkCell * neighbor = m_vtkPolyData->GetCell(neighborCellId);
+        idArray->SetArray(neighbor->GetPointIds()->GetPointer(0), neighbor->GetNumberOfPoints(), true);
+        vtkPolygon::ComputeCentroid(
+            idArray,
+            m_vtkPolyData->GetPoints(),
+            centroid);
+        centroids->SetTuple(neighborCellId++, centroid);
+    }
+
+    m_vtkPolyData->Modified();
+
+    return true;
 }
 
 Qt::ItemFlags QVtkTableModelPolyData::flags(const QModelIndex &index) const
