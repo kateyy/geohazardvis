fbc83801cd9db6e2dd62a17a688940a148799205
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2014-10-08 13:08:49 +0200

    add config GUI for ImageProfilePlot

--- core/data_objects/ImageProfilePlot.cpp
+++ core/data_objects/ImageProfilePlot.cpp
@@ -19,14 +19,54 @@ ImageProfilePlot::ImageProfilePlot(ImageProfileData * dataObject)
 
 PropertyGroup * ImageProfilePlot::createConfigGroup()
 {
-    return new PropertyGroup();
+    PropertyGroup * renderSettings = new PropertyGroup();
+
+    auto * color = renderSettings->addProperty<Color>("color",
+        [this] () {
+        double * color = renderProperty()->GetColor();
+        return Color(static_cast<int>(color[0] * 255), static_cast<int>(color[1] * 255), static_cast<int>(color[2] * 255));
+    },
+        [this] (const Color & color) {
+        renderProperty()->SetColor(color.red() / 255.0, color.green() / 255.0, color.blue() / 255.0);
+        emit geometryChanged();
+    });
+
+    auto * lineWidth = renderSettings->addProperty<unsigned>("lineWidth",
+        [this] () {
+        return static_cast<unsigned>(renderProperty()->GetLineWidth());
+    },
+        [this] (unsigned width) {
+        renderProperty()->SetLineWidth(width);
+        emit geometryChanged();
+    });
+    lineWidth->setOption("title", "line width");
+    lineWidth->setOption("minimum", 1);
+    lineWidth->setOption("maximum", std::numeric_limits<unsigned>::max());
+    lineWidth->setOption("step", 1);
+    lineWidth->setOption("suffix", " pixel");
+
+    auto transparency = renderSettings->addProperty<double>("transparency",
+        [this] () {
+        return (1.0 - renderProperty()->GetOpacity()) * 100;
+    },
+        [this] (double transparency) {
+        renderProperty()->SetOpacity(1.0 - transparency * 0.01);
+        emit geometryChanged();
+    });
+    transparency->setOption("minimum", 0);
+    transparency->setOption("maximum", 100);
+    transparency->setOption("step", 1);
+    transparency->setOption("suffix", " %");
+
+    return renderSettings;
 }
 
 vtkProperty * ImageProfilePlot::createDefaultRenderProperty() const
 {
     vtkProperty * property = vtkProperty::New();
     property->LightingOff();
-    property->SetColor(0, 0, 0);
+    property->SetColor(1, 0, 0);
+    property->SetLineWidth(2);
     return property;
 }
 
