dc8699998f8220231415cbb3bb2ae3b655c1803f
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2015-03-08 19:20:13 +0100

    remove t_FP from FileParser, use type from types.h instead

--- core/io/FileParser.cpp
+++ core/io/FileParser.cpp
@@ -14,6 +14,7 @@
 #include <sstream>
 
 using namespace std;
+using namespace io;
 
 namespace FileParser
 {
--- core/io/FileParser.h
+++ core/io/FileParser.h
@@ -7,24 +7,21 @@
 
 #pragma once
 
-#include <vector>
-
 #include <core/core_api.h>
+#include <core/io/types.h>
 
 
 namespace FileParser
 {
 
-using t_FP = double;
-
 bool CORE_API populateIOVectors(const std::string inputFileName,
-    std::vector<std::vector<t_FP> > &ioVectors);
-bool CORE_API parseIOFile(const std::string inputFileName, std::vector<t_FP> &parsedData, size_t & nbColumns);
-bool CORE_API parseIOStream(std::ifstream & inputStream, std::vector<t_FP> &parsedData, size_t numValues);
-void populateVectorsFromData(const std::vector<t_FP> &parsedData,
-    std::vector<std::vector<t_FP> > &vectorizedData);
+    std::vector<std::vector<io::t_FP> > &ioVectors);
+bool CORE_API parseIOFile(const std::string inputFileName, std::vector<io::t_FP> &parsedData, size_t & nbColumns);
+bool CORE_API parseIOStream(std::ifstream & inputStream, std::vector<io::t_FP> &parsedData, size_t numValues);
+void populateVectorsFromData(const std::vector<io::t_FP> &parsedData,
+    std::vector<std::vector<io::t_FP> > &vectorizedData);
 bool populateIOVectors(std::ifstream & inputStream,
-    std::vector<std::vector<t_FP> > &ioVectors,
+    std::vector<std::vector<io::t_FP> > &ioVectors,
     size_t numTuples,
     size_t componentsPerTuple);
 };
