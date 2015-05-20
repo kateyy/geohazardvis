dd6a76c5b0b17972b7a2a8b5a633eb86fd8f9a32
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2015-05-20 14:36:46 +0200

    fix update install/deployment, add PrepareQtDeploy target (manual)

--- packages/helper-qtdeploy.cmake
+++ packages/helper-qtdeploy.cmake
@@ -1,21 +1,23 @@
 
 # copy required Qt binaries to deploy them with installs/packages
 
+set(QT_DEPLOY_DIR ${CMAKE_BINARY_DIR}/qt_deploy)
+
+add_custom_target(PrepareQtDeploy
+    DEPENDS ${META_PROJECT_NAME}
+    COMMAND ${CMAKE_COMMAND} -E remove_directory "${QT_DEPLOY_DIR}"
+    COMMAND windeployqt "${CMAKE_BINARY_DIR}/$<CONFIG>/${META_PROJECT_NAME}$<$<CONFIG:Debug>:d>$<$<CONFIG:RelWithDebInfo>:rd>.exe" --dir "${QT_DEPLOY_DIR}" $<$<CONFIG:Debug>:--debug>$<$<NOT:$<CONFIG:Debug>>:--release>
+    VERBATIM
+)
+
 function(deployQtBinariesForTarget TARGET_NAME)
 
     if(MSVC AND CPACK_INSTALL_3RDPARTY_DLLS)
 
-        set(QT_DEPLOY_DIR ${CMAKE_BINARY_DIR}/qt_deploy)
-
-        set(EXE ${CMAKE_BINARY_DIR}/$<CONFIG>/${META_PROJECT_NAME}$<$<CONFIG:Debug>:d>$<$<CONFIG:RelWithDebInfo>:rd>.exe)
-        
-        add_custom_command(TARGET ${TARGET_NAME} PRE_BUILD
-            COMMAND ${CMAKE_COMMAND} ARGS -E remove_directory "${QT_DEPLOY_DIR}"
-            COMMAND windeployqt ARGS "${EXE}" --dir "${QT_DEPLOY_DIR}" $<$<CONFIG:Debug>:--debug>$<$<NOT:$<CONFIG:Debug>>:--release>
-            VERBATIM)
+        add_dependencies(${TARGET_NAME} PrepareQtDeploy)
 
         install(DIRECTORY "${QT_DEPLOY_DIR}/" DESTINATION ${INSTALL_BIN})
-        
+
     endif()
-    
+
 endfunction()
