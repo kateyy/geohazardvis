6a9be43433b6bcff329095dd5d4953153fbba263
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2015-06-01 20:00:31 +0200

    Fix CMake warnings / 2.8.12 compatible on Linux

--- packages/helper-qtdeploy.cmake
+++ packages/helper-qtdeploy.cmake
@@ -3,16 +3,21 @@
 
 set(QT_DEPLOY_DIR ${CMAKE_BINARY_DIR}/qt_deploy)
 
-add_custom_target(PrepareQtDeploy
-    DEPENDS ${META_PROJECT_NAME}
-    COMMAND ${CMAKE_COMMAND} -E remove_directory "${QT_DEPLOY_DIR}"
-    COMMAND windeployqt "${CMAKE_BINARY_DIR}/$<CONFIG>/${META_PROJECT_NAME}$<$<CONFIG:Debug>:d>$<$<CONFIG:RelWithDebInfo>:rd>.exe" --dir "${QT_DEPLOY_DIR}" $<$<CONFIG:Debug>:--debug>$<$<NOT:$<CONFIG:Debug>>:--release>
-    VERBATIM
-)
+if(WIN32 AND ${CMAKE_MAJOR_VERSION} VERSION_GREATER 3)
+    add_custom_target(PrepareQtDeploy
+        DEPENDS ${META_PROJECT_NAME}
+        COMMAND ${CMAKE_COMMAND} -E remove_directory "${QT_DEPLOY_DIR}"
+        COMMAND windeployqt "${CMAKE_BINARY_DIR}/$<CONFIG>/${META_PROJECT_NAME}$<$<CONFIG:Debug>:d>$<$<CONFIG:RelWithDebInfo>:rd>.exe" --dir "${QT_DEPLOY_DIR}" $<$<CONFIG:Debug>:--debug>$<$<NOT:$<CONFIG:Debug>>:--release>
+        VERBATIM
+    )
+elseif(WIN32)
+    # WIN32 and old CMake
+    message(warning "Automated Qt is only implemented for CMake version >= 3.0!")
+endif()
 
 function(deployQtBinariesForTarget TARGET_NAME)
 
-    if(MSVC AND CPACK_INSTALL_3RDPARTY_DLLS)
+    if(WIN32 AND CPACK_INSTALL_3RDPARTY_DLLS AND ${CMAKE_MAJOR_VERSION} VERSION_GREATER 3)
 
         add_dependencies(${TARGET_NAME} PrepareQtDeploy)
 
