197b6c7acc9ea0168d5d426faa3f9ffd6810329c
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2015-11-06 15:36:00 +0100

    add QString, vtkVector, std::array conversion utility header

---
+++ core/utility/conversions.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <array>
+
+#include <QString>
+
+#include <vtkVector.h>
+
+#include <core/io/types.h>
+
+
+template<typename FPType, size_t Size>
+QString arrayToString(const std::array<FPType, Size> & a);
+
+template<typename FPType, size_t Size>
+void stringToArray(const QString & s, std::array<FPType, Size> & resultArray);
+
+template<typename FPType, size_t Size>
+std::array<FPType, Size> stringToArray(const QString & s);
+
+template<typename FPType>
+void stringToVector3(const QString & s, vtkVector3<FPType> & resultVector);
+
+template<typename FPType>
+vtkVector3<FPType> stringToVector3(const QString & s);
+
+template<typename FPType>
+QString vector3ToString(const vtkVector3<FPType> & v);
+
+
+#include "conversions.hpp"
---
+++ core/utility/conversions.hpp
@@ -0,0 +1,77 @@
+#include <core/utility/conversions.h>
+
+
+template<typename FPType, size_t Size>
+QString arrayToString(const std::array<FPType, Size> & a)
+{
+    if (Size == 0)
+        return{};
+
+    QString result;
+    for (auto v : a)
+        result += QString::number(v) + " ";
+    result.truncate(result.length() - 1);
+
+    return result;
+}
+
+template<typename FPType, size_t Size>
+void stringToArray(const QString & s, std::array<FPType, Size> & resultArray)
+{
+    auto parts = s.split(" ");
+    if (parts.size() < Size)
+    {
+        resultArray = {};
+        return;
+    }
+
+    for (int i = 0; i < Size; ++i)
+    {
+        resultArray[i] = static_cast<FPType>(parts[i].toDouble());
+    }
+}
+
+template<typename FPType, size_t Size>
+std::array<FPType, Size> stringToArray(const QString & s)
+{
+    std::array<FPType, Size> result = {};
+    stringToArray(s, result);
+    return result;
+}
+
+template<typename FPType>
+void stringToVector3(const QString & s, vtkVector3<FPType> & resultVector)
+{
+    auto array = stringToArray<FPType, 3u>(s);
+
+    if (array.size() != 3u)
+    {
+        resultVector = {};
+        return;
+    }
+
+    resultVector.Set(array[0], array[1], array[2]);
+}
+
+template<typename FPType>
+vtkVector3<FPType> stringToVector3(const QString & s)
+{
+    vtkVector3<FPType> result;
+
+    stringToVector3(s, result);
+
+    return result;
+}
+
+template<typename FPType>
+QString vector3ToString(const vtkVector3<FPType> & v)
+{
+    std::array<FPType, 3u> a;
+
+    for (size_t i = 0; i < 3u; ++i)
+    {
+        a[i] = v[static_cast<int>(i)];
+    }
+
+    return arrayToString(a);
+}
