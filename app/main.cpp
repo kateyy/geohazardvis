53222d7a2db3d705eae42b9ad6f939298b0fa0ee
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2015-11-06 15:35:25 +0100

    fix finding of Qt Windows platform plugin when starting from command line

--- app/main.cpp
+++ app/main.cpp
@@ -1,26 +1,60 @@
 #include "Application.h"
 
+#if defined(_MSC_VER)
+#include <Windows.h>
+#endif
+
+
+void initializeLibraryPath();
+
 
 int main(int argc, char** argv)
 {
-#if defined(WIN32)
-    auto applicationPath = QString::fromLocal8Bit(argv[0]);
-    applicationPath.truncate(applicationPath.lastIndexOf('\\'));
-    applicationPath.replace('\\', '/');
-
-    // search for libraries deployed with the application first
-    QStringList libraryPaths;
-    libraryPaths << applicationPath;
-
-    if (!QCoreApplication::libraryPaths().isEmpty())
-        libraryPaths << QCoreApplication::libraryPaths().first();
-
-    QCoreApplication::setLibraryPaths(libraryPaths);
-#endif
+    initializeLibraryPath();
 
     Application app(argc, argv);
 
     app.startup();
 
     return app.exec();
+}
+
+void initializeLibraryPath()
+{
+#if defined(_MSC_VER)
+    // "By convention, argv [0] is the command with which the program is invoked. (...)"
+    // E.g., this may be the exe name only (without the path) when invoked from a command line.
+    // So get the correct binary path via Win32 API
+    TCHAR win_applicationPath[_MAX_PATH + 1];
+    auto pathLength = GetModuleFileName(NULL, win_applicationPath, _MAX_PATH);
+    if (pathLength == ERROR_INSUFFICIENT_BUFFER)
+    {
+        printf("Cannot determine the application path: insufficient buffer");
+        return;
+    }
+
+#if TCHAR == WCHAR
+    QString applicationPath = QString::fromWCharArray(win_applicationPath, pathLength);
+#else
+    QString applicationPath = QString::fromLatin1(win_applicationPath, pathLength);
+#endif
+
+    applicationPath.truncate(applicationPath.lastIndexOf('\\'));
+    applicationPath.replace('\\', '/');
+    if (applicationPath.isEmpty())
+    {
+        applicationPath = ".";
+    }
+
+    // search for libraries deployed with the application first
+    QStringList libraryPaths;
+    libraryPaths << applicationPath;
+
+    if (!QCoreApplication::libraryPaths().isEmpty())
+    {
+        libraryPaths << QCoreApplication::libraryPaths().first();
+    }
+
+    QCoreApplication::setLibraryPaths(libraryPaths);
+#endif
 }
