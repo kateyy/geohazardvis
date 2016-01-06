21372e73284b59960a2d9f8b27ab32ae1685c945
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2016-01-06 19:53:55 +0100

    export uchar images without color remapping, check for valid scalar data

--- core/io/Exporter.cpp
+++ core/io/Exporter.cpp
@@ -1,12 +1,15 @@
 #include <core/io/Exporter.h>
 
 #include <cassert>
+#include <limits>
 
 #include <QFileInfo>
 #include <QMap>
 #include <QString>
 
+#include <vtkDataArray.h>
 #include <vtkDataSet.h>
+#include <vtkPointData.h>
 
 #include <vtkImageMapToColors.h>
 #include <vtkLookupTable.h>
@@ -94,21 +97,53 @@ bool Exporter::exportImageFormat(ImageDataObject * image, const QString & fileNa
 
     assert(writer);
 
-    auto toUChar = vtkSmartPointer<vtkImageMapToColors>::New();
-    toUChar->SetInputData(image->dataSet());
-    toUChar->SetOutputFormatToLuminance();
+    const auto scalars = image->dataSet()->GetPointData()->GetScalars();
+    if (!scalars)
+    {
+        return false;
+    }
 
-    auto lut = vtkSmartPointer<vtkLookupTable>::New();
-    const double * range = image->minMaxValue();
-    lut->SetTableRange(range[0], range[1]);
-    lut->SetNumberOfTableValues(0xFF);
-    lut->SetHueRange(0, 0);
-    lut->SetSaturationRange(0, 0);
-    lut->SetValueRange(0, 1);
+    const auto components = scalars->GetNumberOfComponents();
+    if (components != 1 && components != 3 && components != 4)
+    {
+        return false;
+    }
 
-    toUChar->SetLookupTable(lut);
+    if (scalars->GetDataType() == VTK_UNSIGNED_CHAR)
+    {
+        writer->SetInputData(image->dataSet());
+    }
+    else
+    {
+        auto toUChar = vtkSmartPointer<vtkImageMapToColors>::New();
+        toUChar->SetInputData(image->dataSet());
 
-    writer->SetInputConnection(toUChar->GetOutputPort());
+        auto lut = vtkSmartPointer<vtkLookupTable>::New();
+        lut->SetNumberOfTableValues(0xFF);
+        lut->SetHueRange(0, 0);
+        lut->SetSaturationRange(0, 0);
+        lut->SetValueRange(0, 1);
+
+        double totalRange[2] = { std::numeric_limits<double>::max(), std::numeric_limits<double>::lowest() };
+            
+        for (int c = 0; c < components; ++c)
+        {
+            double range[2];
+            scalars->GetRange(range, c);
+            totalRange[0] = std::min(totalRange[0], range[0]);
+            totalRange[1] = std::min(totalRange[1], range[1]);
+        }
+
+        toUChar->SetOutputFormat(
+            components == 3 ? VTK_RGB :
+            (components == 4 ? VTK_RGBA :
+                VTK_LUMINANCE));
+
+        toUChar->SetLookupTable(lut);
+
+        writer->SetInputConnection(toUChar->GetOutputPort());
+    }
+
     writer->SetFileName(fileName.toUtf8().data());
     writer->Write();
 
