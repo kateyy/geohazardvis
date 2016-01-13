77e3d760106665a42fcb42439527ef02546024ad
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2016-01-13 11:47:37 +0100

    manually deploy QtOpenGL DLLs on windows

--- packages/helper-qtdeploy.cmake
+++ packages/helper-qtdeploy.cmake
@@ -4,16 +4,24 @@
 set(QT_DEPLOY_DIR ${CMAKE_BINARY_DIR}/qt_deploy)
 
 if(WIN32 AND ${CMAKE_VERSION} VERSION_GREATER 3.0)
+
+    # deprecated Qt5 OpenGL module is not deployed by qt anymore
+    get_target_property(QtOpenGL_release_location Qt5::OpenGL IMPORTED_LOCATION_RELEASE)
+    get_target_property(QtOpenGL_debug_location   Qt5::OpenGL IMPORTED_LOCATION_DEBUG)
+
     add_custom_target(PrepareQtDeploy
         DEPENDS ${META_PROJECT_NAME}
         COMMAND ${CMAKE_COMMAND} -E remove_directory "${QT_DEPLOY_DIR}"
         COMMAND windeployqt 
-            ${CMAKE_BINARY_DIR}/$<CONFIG>/${META_PROJECT_NAME}$<$<CONFIG:Debug>:d>$<$<CONFIG:RelWithDebInfo>:rd>.exe
-            --dir ${QT_DEPLOY_DIR}
+            "${CMAKE_BINARY_DIR}/$<CONFIG>/${META_PROJECT_NAME}$<$<CONFIG:Debug>:d>$<$<CONFIG:RelWithDebInfo>:rd>.exe"
+            --dir "${QT_DEPLOY_DIR}"
             $<$<CONFIG:Debug>:--debug>$<$<CONFIG:RelWithDebInfo>:--release-with-debug-info>$<$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRelease>>:--release>
             --no-translations
             --no-compiler-runtime
             --no-system-d3d-compiler
+        COMMAND ${CMAKE_COMMAND} -E copy
+            "$<$<NOT:$<CONFIG:Debug>>:${QtOpenGL_release_location}>$<$<CONFIG:DEBUG>:${QtOpenGL_debug_location}>"
+            "${QT_DEPLOY_DIR}"
         VERBATIM
     )
     set_target_properties(PrepareQtDeploy 
