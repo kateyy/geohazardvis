a3db18ad921f1dff6bdab9de8d0f26bceef484ef
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2016-01-29 16:05:04 +0100

    rework DataExtent helper class, use std::array as storage

--- core/utility/DataExtent.h
+++ core/utility/DataExtent.h
@@ -1,43 +1,55 @@
 #pragma once
 
+#include <array>
+
 #include <vtkVector.h>
 
 
-template<typename T, size_t Dimensions = 3>
+template<typename T, size_t Dimensions = 3u>
 class DataExtent
 {
 public:
+    static const auto ValueCount = Dimensions * 2u;
+    using array_t = std::array<T, ValueCount>;
+
     DataExtent();
-    explicit DataExtent(T extent[Dimensions * 2]);
+    explicit DataExtent(array_t extent);
+    explicit DataExtent(const T extent[ValueCount]);
 
-    void Add(const T other[Dimensions * 2]);
+    bool isEmpty() const;
 
-    void Add(const DataExtent & other);
+    operator array_t () const;
 
     T operator[] (size_t index) const;
-
     T & operator[] (size_t index);
 
-    bool operator==(const T other[Dimensions * 2]) const;
-
+    bool operator==(const T other[ValueCount]) const;
     bool operator==(const DataExtent & other) const;
 
-    vtkVector<T, Dimensions> Center() const;
-    vtkVector<T, Dimensions> Size() const;
+    vtkVector<T, Dimensions> center() const;
+    vtkVector<T, Dimensions> size() const;
 
     template<typename newT, size_t newDimensions>
-    DataExtent<newT, newDimensions> ConvertTo() const;
+    DataExtent<newT, newDimensions> convertTo() const;
 
-    const T * Data() const;
-
+    const T * data() const;
     // unsafe workaround for non-const VTK interfaces (e.g., vtkImageData)
-    T * Data();
+    T * data();
+
+
+    void add(const DataExtent & other);
+    void add(const T other[ValueCount]);
+
+    static DataExtent intersect(DataExtent lhs, const DataExtent & rhs);
+
+    DataExtent & intersect(const DataExtent & other);
 
 private:
-    T m_extent[Dimensions * 2];
+    array_t m_extent;
 };
 
 using ImageExtent = DataExtent<int, 3>;
+using DataBounds = DataExtent<double, 3>;
 
 
 #include <core/utility/DataExtent.hpp>
--- core/utility/DataExtent.hpp
+++ core/utility/DataExtent.hpp
@@ -1,15 +1,17 @@
+#pragma once
 
 #include <core/utility/DataExtent.h>
 
 #include <algorithm>
-#include <cassert>
-#include <cmath>
 #include <limits>
+#include <type_traits>
 
 
 template<typename T, size_t Dimensions>
 DataExtent<T, Dimensions>::DataExtent()
 {
+    static_assert(size_t(int(Dimensions)) == Dimensions, "Data Extent type is not compatible with VTK's vector type");
+
     for (size_t i = 0; i < Dimensions; ++i)
     {
         m_extent[2 * i] = std::numeric_limits<T>::max();
@@ -18,48 +20,56 @@ DataExtent<T, Dimensions>::DataExtent()
 }
 
 template<typename T, size_t Dimensions>
-DataExtent<T, Dimensions>::DataExtent(T extent[Dimensions * 2])
+DataExtent<T, Dimensions>::DataExtent(array_t extent)
+    : m_extent(extent)
 {
-    std::copy(extent, extent + Dimensions * 2, m_extent);
 }
 
 template<typename T, size_t Dimensions>
-void DataExtent<T, Dimensions>::Add(const T other[Dimensions * 2])
+DataExtent<T, Dimensions>::DataExtent(const T extent[ValueCount])
 {
-    for (size_t i = 0; i < Dimensions; ++i)
+    for (size_t i = 0; i < ValueCount; ++i)
     {
-        if (m_extent[2 * i] < other[2 * i])
-            m_extent[2 * i] = other[2 * i];
-
-        if (m_extent[2 * i + 1] > other[2 * i + 1])
-            m_extent[2 * i + 1] = other[2 * i + 1];
+        m_extent[i] = extent[i];
     }
 }
 
 template<typename T, size_t Dimensions>
-void DataExtent<T, Dimensions>::Add(const DataExtent & other)
+bool DataExtent<T, Dimensions>::isEmpty() const
 {
-    Add(other.m_extent);
+    for (size_t i = 0; i < Dimensions; ++i)
+    {
+        if (m_extent[2 * i] > m_extent[2 * i + 1])
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+template<typename T, size_t Dimensions>
+DataExtent<T, Dimensions>::operator array_t() const
+{
+    return m_extent;
 }
 
 template<typename T, size_t Dimensions>
 T DataExtent<T, Dimensions>::operator[] (size_t index) const
 {
-    assert(index < Dimensions * 2);
     return m_extent[index];
 }
 
 template<typename T, size_t Dimensions>
 T & DataExtent<T, Dimensions>::operator[] (size_t index)
 {
-    assert(index < Dimensions * 2);
     return m_extent[index];
 }
 
 template<typename T, size_t Dimensions>
-bool DataExtent<T, Dimensions>::operator==(const T other[Dimensions * 2]) const
+bool DataExtent<T, Dimensions>::operator==(const T other[ValueCount]) const
 {
-    for (size_t i = 0; i < Dimensions * 2; ++i)
+    for (size_t i = 0; i < ValueCount; ++i)
     {
         if (m_extent[i] != other[i])
             return false;
@@ -70,24 +80,24 @@ bool DataExtent<T, Dimensions>::operator==(const T other[Dimensions * 2]) const
 template<typename T, size_t Dimensions>
 bool DataExtent<T, Dimensions>::operator==(const DataExtent & other) const
 {
-    return operator==(other.m_extent);
+    return operator==(other.m_extent.data());
 }
 
 template<typename T, size_t Dimensions>
-const T * DataExtent<T, Dimensions>::Data() const
+const T * DataExtent<T, Dimensions>::data() const
 {
-    return m_extent;
+    return m_extent.data();
 }
 
 template<typename T, size_t Dimensions>
-T * DataExtent<T, Dimensions>::Data()
+T * DataExtent<T, Dimensions>::data()
 {
-    return m_extent;
+    return m_extent.data();
 }
 
 template<typename T, size_t Dimensions>
 template<typename newT, size_t newDimensions>
-DataExtent<newT, newDimensions> DataExtent<T, Dimensions>::ConvertTo() const
+DataExtent<newT, newDimensions> DataExtent<T, Dimensions>::convertTo() const
 {
     DataExtent<newT, newDimensions> result;
 
@@ -102,27 +112,84 @@ DataExtent<newT, newDimensions> DataExtent<T, Dimensions>::ConvertTo() const
 }
 
 template<typename T, size_t Dimensions>
-vtkVector<T, Dimensions> DataExtent<T, Dimensions>::Center() const
+vtkVector<T, Dimensions> DataExtent<T, Dimensions>::center() const
 {
     vtkVector<T, Dimensions> result;
 
     for (size_t i = 0; i < Dimensions; ++i)
     {
-        result[i] = static_cast<T>(0.5 * m_extent[2 * i] + 0.5 * m_extent[2 * i + 1]);
+        auto int_i = static_cast<int>(i);
+        result[int_i] = static_cast<T>(0.5 * m_extent[2 * i] + 0.5 * m_extent[2 * i + 1]);
     }
 
     return result;
 }
 
 template<typename T, size_t Dimensions>
-vtkVector<T, Dimensions> DataExtent<T, Dimensions>::Size() const
+vtkVector<T, Dimensions> DataExtent<T, Dimensions>::size() const
 {
     vtkVector<T, Dimensions> result;
 
     for (size_t i = 0; i < Dimensions; ++i)
     {
-        result[i] = m_extent[2 * i + 1] - m_extent[2 * i];
+        auto int_i = static_cast<int>(i);
+        result[int_i] = m_extent[2 * i + 1] - m_extent[2 * i];
     }
 
     return result;
+}
+
+template<typename T, size_t Dimensions>
+void DataExtent<T, Dimensions>::add(const T other[ValueCount])
+{
+    for (size_t i = 0; i < Dimensions; ++i)
+    {
+        if (m_extent[2 * i] < other[2 * i])
+            m_extent[2 * i] = other[2 * i];
+
+        if (m_extent[2 * i + 1] > other[2 * i + 1])
+            m_extent[2 * i + 1] = other[2 * i + 1];
+    }
+}
+
+template<typename T, size_t Dimensions>
+void DataExtent<T, Dimensions>::add(const DataExtent & other)
+{
+    Add(other.m_extent);
+}
+
+template<typename T, size_t Dimensions>
+auto DataExtent<T, Dimensions>::intersect(DataExtent lhs, const DataExtent & rhs) -> DataExtent
+{
+    lhs.intersect(rhs);
+
+    return lhs;
+}
+
+template<typename T, size_t Dimensions>
+auto DataExtent<T, Dimensions>::intersect(const DataExtent & other) -> DataExtent &
+{
+    if (isEmpty() || other.isEmpty())
+    {
+        return *this;
+    }
+
+    for (size_t i = 0u; i < Dimensions; ++i)
+    {
+        const auto minIdx = 2u * i;
+        const auto maxIdx = 2u * i + 1u;
+
+        if (m_extent[minIdx] < other.m_extent[minIdx])
+            m_extent[minIdx] = other.m_extent[minIdx];
+        if (m_extent[maxIdx] > other.m_extent[maxIdx])
+            m_extent[maxIdx] = other.m_extent[maxIdx];
+
+        if (m_extent[minIdx] > m_extent[maxIdx])
+        {
+            *this = DataExtent();
+            break;
+        }
+    }
+
+    return *this;
 }
