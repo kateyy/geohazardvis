2f88337b87ec114993a8d3af4e89ddedad0498c3
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2016-05-13 14:36:21 +0200

    Don't set Qt::AA_EnableHighDpiScaling attribute on linux
    * work around wrong reported DPI
    * tested on Kubuntu 16.04 only

--- app/main.cpp
+++ app/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
 #if QT_VERSION >= QT_VERSION_CHECK(5, 1, 0)
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
-#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0) && !defined(__linux__)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
