31a3ba96e9856929a0caeb311b79db51b8c0a9e3
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2016-05-20 22:14:39 +0200

    Linux deployment

--- app/main.cpp
+++ app/main.cpp
@@ -1,22 +1,124 @@
-#include "Application.h"
+#include <iostream>
+#include <limits>
 
 #if defined(_MSC_VER)
 #include <Windows.h>
+#elif defined(__linux__)
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <cstdlib>
 #endif
 
+#include "Application.h"
 #include "config.h"
 
+namespace
+{
 
-void initializeLibraryPath();
+#if defined(_MSC_VER)
+QString pathToSelf()
+{
+    // "By convention, argv [0] is the command with which the program is invoked. (...)"
+    // E.g., this may be the exe name only (without the path) when invoked from a command line.
+    // So get the correct binary path via Win32 API
+    TCHAR win_applicationPath[_MAX_PATH + 1];
+    const auto pathLength = GetModuleFileName(NULL, win_applicationPath, _MAX_PATH);
+    if (pathLength == ERROR_INSUFFICIENT_BUFFER)
+    {
+        std::cerr << "Cannot determine the application path: insufficient buffer" << std::endl;
+        return {};
+    }
+
+#if TCHAR == WCHAR
+    auto applicationPath = QString::fromWCharArray(win_applicationPath, pathLength);
+#else
+    auto applicationPath = QString::fromLatin1(win_applicationPath, pathLength);
+#endif
+
+    applicationPath.replace('\\', '/');
+
+    return applicationPath;
+}
+
+#elif defined(__linux__)
+QString pathToSelf()
+{
+    // http://stackoverflow.com/questions/1023306/finding-current-executables-path-without-proc-self-exe
+    // http://man7.org/linux/man-pages/man2/readlink.2.html
+    const char * exeSymLink = "/proc/self/exe";
+    struct stat sb;
+
+    ssize_t r;
+    if (stat(exeSymLink, &sb) == -1)
+    {
+        std::cerr << "lstat() failed on \"" << exeSymLink << std::endl;
+        return {};
+    }
+
+    if (sb.st_size + 1 > static_cast<decltype(sb.st_size)>(std::numeric_limits<int>::max()))
+    {
+        std::cerr << "Path to executable is too long (for Qt)." << std::endl;
+        return {};
+    }
+
+    QByteArray linkname(static_cast<int>(sb.st_size + 1), '\0');
+    r = readlink(exeSymLink, linkname.data(), sb.st_size + 1);
+
+    if (r == -1)
+    {
+        std::cerr << "readlink() failed on \"" << exeSymLink << std::endl;
+        return {};
+    }
+
+    if (r > sb.st_size)
+    {
+        std::cerr << "symlink increased in size between lstat() and readlink()" << std::endl;
+        return {};
+    }
+
+    return QString::fromUtf8(linkname);
+}
+
+#else
+QString pathToSelf()
+{
+    return {};
+}
+#endif
+
+void initializeLibraryPath()
+{
+    auto applicationPath = pathToSelf();
+
+    if (applicationPath.isEmpty())
+    {
+        std::cerr << "Could not determin the executable location. Cannot ensure to load correct Qt libraries and plugins." << std::endl;
+        return;
+    }
+
+    applicationPath.truncate(applicationPath.lastIndexOf('/'));
+
+    // search for libraries deployed with the application first
+    QStringList libraryPaths;
+    libraryPaths << applicationPath;
+
+    for (auto && path : QCoreApplication::libraryPaths())
+    {
+        libraryPaths << path;
+    }
+
+    QCoreApplication::setLibraryPaths(libraryPaths);
+}
+
+}
 
 
 int main(int argc, char** argv)
 {
     initializeLibraryPath();
 
-#if QT_VERSION >= QT_VERSION_CHECK(5, 1, 0)
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-#endif
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0) && !defined(__linux__)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
@@ -28,44 +130,4 @@ int main(int argc, char** argv)
     app.startup();
 
     return app.exec();
-}
-
-void initializeLibraryPath()
-{
-#if defined(_MSC_VER)
-    // "By convention, argv [0] is the command with which the program is invoked. (...)"
-    // E.g., this may be the exe name only (without the path) when invoked from a command line.
-    // So get the correct binary path via Win32 API
-    TCHAR win_applicationPath[_MAX_PATH + 1];
-    auto pathLength = GetModuleFileName(NULL, win_applicationPath, _MAX_PATH);
-    if (pathLength == ERROR_INSUFFICIENT_BUFFER)
-    {
-        printf("Cannot determine the application path: insufficient buffer");
-        return;
-    }
-
-#if TCHAR == WCHAR
-    QString applicationPath = QString::fromWCharArray(win_applicationPath, pathLength);
-#else
-    QString applicationPath = QString::fromLatin1(win_applicationPath, pathLength);
-#endif
-
-    applicationPath.truncate(applicationPath.lastIndexOf('\\'));
-    applicationPath.replace('\\', '/');
-    if (applicationPath.isEmpty())
-    {
-        applicationPath = ".";
-    }
-
-    // search for libraries deployed with the application first
-    QStringList libraryPaths;
-    libraryPaths << applicationPath;
-
-    if (!QCoreApplication::libraryPaths().isEmpty())
-    {
-        libraryPaths << QCoreApplication::libraryPaths().first();
-    }
-
-    QCoreApplication::setLibraryPaths(libraryPaths);
-#endif
 }
--- packages/helper-qtdeploy.cmake
+++ packages/helper-qtdeploy.cmake
@@ -1,11 +1,24 @@
 
 # copy required Qt binaries to deploy them with installs/packages
 
+if (CMAKE_VERSION VERSION_LESS 3.0)
+    message(WARNING "Automated Qt deployment is only available with CMake version >= 3.0.")
+endif()
+
+if (CMAKE_VERSION VERSION_LESS 3.0 OR NOT OPTION_INSTALL_3RDPARTY_BINARIES)
+
+    function(deployQtBinariesForTarget TARGET_NAME)
+    endfunction()
+
+    return()
+
+endif()
+
+
 set(QT_DEPLOY_DIR ${CMAKE_BINARY_DIR}/qt_deploy)
 
-if(WIN32 AND ${CMAKE_VERSION} VERSION_GREATER 3.0)
-
-    # deprecated Qt5 OpenGL module is not deployed by qt anymore
+if (WIN32)
+    # deprecated Qt5 OpenGL module is not deployed by Qt anymore
     get_target_property(QtOpenGL_release_location Qt5::OpenGL IMPORTED_LOCATION_RELEASE)
     get_target_property(QtOpenGL_debug_location   Qt5::OpenGL IMPORTED_LOCATION_DEBUG)
 
@@ -28,19 +41,133 @@ if(WIN32 AND ${CMAKE_VERSION} VERSION_GREATER 3.0)
         PROPERTIES
         FOLDER  "${IDE_FOLDER}"
     )
-elseif(WIN32)
-    # WIN32 and old CMake
-    message(WARNING "Automated Qt is only implemented for CMake version >= 3.0!")
+
+elseif(UNIX)
+
+    # qt.conf: setup deploy folder structure
+    install(FILES qt.conf.linux
+        DESTINATION ${INSTALL_BIN}
+        RENAME qt.conf
+    )
+
+    function(getTruncatedFileName _longName _cutLength _resultVar)
+        string(LENGTH ${_longName} _length)
+        math(EXPR _length "${_length} - ${_cutLength}")
+        string(SUBSTRING ${_longName} 0 ${_length} _truncated)
+        get_filename_component(_truncated ${_truncated} NAME)
+        set(${_resultVar} ${_truncated} PARENT_SCOPE)
+    endfunction()
+
+    # Libraries: need to rename, e.g., "libQt5Core.so.5.6.0" -> "libQt5Core.so.5"
+    foreach(_component ${PROJECT_QT_COMPONENTS})
+        get_target_property(_locationRelease Qt5::${_component} IMPORTED_LOCATION_RELEASE)
+        get_target_property(_locationDebug Qt5::${_component} IMPORTED_LOCATION_DEBUG)
+
+        if (NOT _locationDebug)
+            set(_locationDebug ${_locationRelease})
+        endif()
+
+        getTruncatedFileName(${_locationRelease} 4 _locationReleaseTruncated)
+        getTruncatedFileName(${_locationDebug} 4 _locationDebugTruncated)
+
+        install(FILES ${_locationRelease}
+            DESTINATION ${INSTALL_SHARED}
+            CONFIGURATIONS Release RelWithDebInfo RelNoOptimization
+            RENAME ${_locationReleaseTruncated}
+        )
+        install(FILES ${_locationDebug}
+            DESTINATION ${INSTALL_SHARED}
+            CONFIGURATIONS Debug
+            RENAME ${_locationDebugTruncated}
+        )
+    endforeach()
+
+    set(_allPlugins)
+    foreach(_component ${PROJECT_QT_COMPONENTS})
+        list(APPEND _allPlugins ${Qt5${_component}_PLUGINS})
+    endforeach()
+
+    # Plugins: copy as is [can't check what is actually needed]
+    foreach(_plugin ${_allPlugins})
+        get_target_property(_locationRelease ${_plugin} IMPORTED_LOCATION_RELEASE)
+        get_target_property(_locationDebug ${_plugin} IMPORTED_LOCATION_DEBUG)
+
+        get_filename_component(_directory ${_locationRelease} DIRECTORY)
+        get_filename_component(_pluginCategory ${_directory} NAME)
+
+#         if (${_locationRelease} MATCHES "^.*/platforms/.*$")
+            # only need to deploy this platform plugin. And it has to be located at a fixed position relative to the executable.
+#             if (NOT _plugin STREQUAL "Qt5::QXcbIntegrationPlugin")
+#                 continue()
+#             endif()
+#             set(_pluginInstallDir ${INSTALL_BIN}/platforms)
+#         else()
+#             set(_pluginInstallDir ${INSTALL_SHARED})
+#         endif()
+
+        if (NOT _locationDebug)
+            set(_locationDebug ${_locationRelease})
+        endif()
+
+        install(FILES ${_locationRelease}
+            DESTINATION ${INSTALL_SHARED}/${_pluginCategory}
+            CONFIGURATIONS Release RelWithDebInfo RelNoOptimization
+        )
+        install(FILES ${_locationDebug}
+            DESTINATION ${INSTALL_SHARED}/${_pluginCategory}
+            CONFIGURATIONS Debug
+        )
+    endforeach()
+
+
+    # ICU: no Qt-CMake support at all.
+    # Find all ICU libs shipped with Qt (hopefully not something else..) and copy to shortened names.
+    get_target_property(_qtCoreLibLocation Qt5::Core IMPORTED_LOCATION_RELEASE)
+    get_filename_component(_qtLibDir ${_qtCoreLibLocation} DIRECTORY)
+    file(GLOB _icuPaths
+        LIST_DIRECTORIES false
+        ${_qtLibDir}/*icu*)
+    set(_realIcuPaths)
+    foreach(_icuLib ${_icuPaths})
+        get_filename_component(_realLibPath ${_icuLib} REALPATH)
+        list(APPEND _realIcuPaths ${_realLibPath})
+    endforeach()
+    list(REMOVE_DUPLICATES _realIcuPaths)
+    foreach(_icuLib ${_realIcuPaths})
+        getTruncatedFileName(${_icuLib} 2 _icuLibTruncated)
+        install(FILES ${_icuLib}
+            DESTINATION ${INSTALL_SHARED}
+            RENAME ${_icuLibTruncated}
+        )
+    endforeach()
+
+    # Platform plugin: not listed in the CMakes' WTF??
+    file(GLOB _XcbQpa_paths
+        LIST_DIRECTORIES false
+        ${_qtLibDir}/*XcbQpa*so*)
+    if (NOT _XcbQpa_paths)
+        message(FATAL_ERROR "Qt Platform plugin \"XcbQpa\" not found!")
+    endif()
+    set(_XcbQpa_path)
+    foreach(_plugin ${_XcbQpa_paths})
+        get_filename_component(_realPath ${_plugin} REALPATH)
+        list(APPEND _XcbQpa_path ${_realPath})
+    endforeach()
+    list(REMOVE_DUPLICATES _XcbQpa_path)
+    list(LENGTH _XcbQpa_path _num_XcbQpa_paths)
+    if (NOT 1 EQUAL _num_XcbQpa_paths)
+        message(FATAL_ERROR "Unexpected number of Qt Platform plugin libraries found (\"XcbQpa\", ${_num_XcbQpa_paths})")
+    endif()
+    getTruncatedFileName(${_XcbQpa_path} 4 _XcbQpa_pathTruncated)
+    install(FILES ${_XcbQpa_path}
+        DESTINATION ${INSTALL_SHARED}
+        RENAME ${_XcbQpa_pathTruncated}
+    )
 endif()
 
 function(deployQtBinariesForTarget TARGET_NAME)
-
-    if(WIN32 AND CPACK_INSTALL_3RDPARTY_DLLS AND ${CMAKE_VERSION} VERSION_GREATER 3.0)
-
+    if (WIN32)
         add_dependencies(${TARGET_NAME} PrepareQtDeploy)
-
-        install(DIRECTORY "${QT_DEPLOY_DIR}/" DESTINATION ${INSTALL_BIN})
-
+        install(DIRECTORY "${QT_DEPLOY_DIR}/" DESTINATION ${INSTALL_SHARED})
     endif()
-
 endfunction()
