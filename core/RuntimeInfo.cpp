b7de69a342edd1e3b7b8db7500a8b8c1c78781db
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2016-06-01 19:55:56 +0200

    Fix RuntimeInfo::basePath logic for windows

--- core/RuntimeInfo.cpp
+++ core/RuntimeInfo.cpp
@@ -13,35 +13,35 @@ const QString & RuntimeInfo::basePath()
     {
         const auto appPath = QCoreApplication::applicationDirPath();
         const auto installBinRelPath = QString(config::installBinRelativePath);
+        const QString checkDirName = "data";
 
         QString _basePath;
 
         // mainly Windows: executable located directly in base folder
         if (installBinRelPath == ".")
         {
-            _basePath = appPath;
+            if (QDir(appPath).exists(checkDirName))
+            {
+                _basePath = appPath;
+            }
         }
         // mainly Linux: executable placed in something like "bin" subfolder
-        else if (appPath.length() >= installBinRelPath.length()
-            && appPath.rightRef(installBinRelPath.length()) == installBinRelPath)
-        {
-            // /some/path/to/base_dir/bin/dir [/exename]
-            // /some/path_to/base_dir <-> / <-> bin/dir
-            _basePath = appPath.left(appPath.length() - (installBinRelPath.length() + 1));
-        }
-        // dev: folder structure not setup yet
         else
+        {
+            if (appPath.length() >= installBinRelPath.length()
+                && appPath.rightRef(installBinRelPath.length()) == installBinRelPath)
+            {
+                // /some/path/to/base_dir/bin/dir [/exename]
+                // /some/path_to/base_dir <-> / <-> bin/dir
+                _basePath = appPath.left(appPath.length() - (installBinRelPath.length() + 1));
+            }
+        }
+
+        // dev: folder structure not setup yet
+        if (_basePath.isEmpty())
         {
             _basePath = QDir::currentPath();
             qDebug() << "RuntimeInfo: Assuming to be running in out of source build. Using current working directory as base path.";
-        }
-
-        if (_basePath.isEmpty() || !QDir(_basePath).exists())
-        {
-            qCritical() << "Could not determine base installation dir.";
-            qCritical() << "QCoreApplication::applicationDirPath():" << appPath;
-            qCritical() << "config::installBin:" << installBinRelPath;
-            _basePath = ".";
         }
 
         return _basePath;
