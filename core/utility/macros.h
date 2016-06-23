8c21df4b2e2056c09f9ecdf7e04cd649b49164be
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2016-06-24 01:13:20 +0200

    Add somewhat portable pragma warning macro

--- core/utility/macros.h
+++ core/utility/macros.h
@@ -6,3 +6,15 @@
 #else
 #define DEBUG_ONLY(S) S
 #endif
+
+
+// https://stackoverflow.com/questions/471935/user-warnings-on-msvc-and-gcc
+// Use: #pragma message WARN("My message")
+#define STRINGISE_IMPL(x) #x
+#define STRINGISE(x) STRINGISE_IMPL(x)
+#if _MSC_VER
+#   define FILE_LINE_LINK __FILE__ "(" STRINGISE(__LINE__) ") : "
+#   define WARN(exp) (FILE_LINE_LINK "WARNING: " exp)
+#else//__GNUC__ - may need other defines for different compilers
+#   define WARN(exp) ("WARNING: " exp)
+#endif
