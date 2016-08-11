kateyy/geohazardvis

commit b711c4e1d04062d7824bdf033befb1fad254eca4

Set opacity to 0 for images with disabled color mapping

core/rendered_data/RenderedImageData.cpp
@@ -145,21 +145,16 @@ void RenderedImageData::scalarsForColorMappingChangedEvent()
         m_mapper->SetInputConnection(colorMappingInput());
     }
 
-    // hack: how to enforce to use color data directly?
-    if (m_colorMappingData->dataMinValue() == m_colorMappingData->dataMaxValue())
-        property()->SetLookupTable(nullptr);
-    else
-        property()->SetLookupTable(m_gradient);
+    property()->SetOpacity(colorMapping().isEnabled()
+        ? 1.0
+        : 0.0);
 }
 
 void RenderedImageData::colorMappingGradientChangedEvent()
 {
-    RenderedData::scalarsForColorMappingChangedEvent();
+    RenderedData::colorMappingGradientChangedEvent();
 
-    if (m_colorMappingData && (m_colorMappingData->dataMinValue() == m_colorMappingData->dataMaxValue()))
-        property()->SetLookupTable(nullptr);
-    else
-        property()->SetLookupTable(m_gradient);
+    property()->SetLookupTable(m_gradient);
 }
 
 void RenderedImageData::visibilityChangedEvent(bool visible)

commit a1cb29f493c3270afd3018c861316ab8d913e7c8

Fix DirectImageColors regression: don't map scalars
* bug introduced in b711c4e1d04062d7824bdf033befb1fad254eca4
* RenderedImageData needs to remove the lookup table from its property in order to disable color mapping when using direct colors

core/color_mapping/ColorMappingData.cpp
@@ -11,12 +11,15 @@
 #include <core/utility/macros.h>
 
 
-ColorMappingData::ColorMappingData(const QList<AbstractVisualizedData *> & visualizedData, int numDataComponents)
+ColorMappingData::ColorMappingData(const QList<AbstractVisualizedData *> & visualizedData,
+    int numDataComponents,
+    bool mapsScalarsToColors)
     : m_isValid{ false }
     , m_visualizedData(visualizedData)
     , m_numDataComponents{ numDataComponents }
     , m_dataComponent{ 0 }
     , m_isActive{ false }
+    , m_mapsScalarsToColors{ mapsScalarsToColors }
     , m_dataMinValue(numDataComponents, std::numeric_limits<double>::max())
     , m_dataMaxValue(numDataComponents, std::numeric_limits<double>::lowest())
     , m_minValue(numDataComponents, std::numeric_limits<double>::max())
@@ -95,6 +98,11 @@ void ColorMappingData::setDataComponent(int component)
         m_lut->SetTableRange(minValue(), maxValue());
         m_lut->Build();
     }
+}
+
+bool ColorMappingData::mapsScalarsToColors() const
+{
+    return m_mapsScalarsToColors;
 }
 
 void ColorMappingData::initialize()

core/color_mapping/ColorMappingData.h
@@ -37,7 +37,9 @@ public:
     static std::vector<std::unique_ptr<ColorMappingData>> newInstance(const QList<AbstractVisualizedData *> & visualizedData);
 
 public:
-    explicit ColorMappingData(const QList<AbstractVisualizedData *> & visualizedData, int numDataComponents = 1);
+    explicit ColorMappingData(const QList<AbstractVisualizedData *> & visualizedData,
+        int numDataComponents = 1,
+        bool mapsScalarsToColors = true);
 
     virtual void activate();
     virtual void deactivate();
@@ -51,6 +53,9 @@ public:
     int numDataComponents() const;
     int dataComponent() const;
     void setDataComponent(int component);
+
+    /** Return whether scalars are mapped/transformed to colors or used directly as color values. */
+    bool mapsScalarsToColors() const;
 
     /** create a filter to map values to color, applying current min/max settings
       * @param dataObject is required to setup object specific parameters on the filter.
@@ -112,6 +117,8 @@ private:
     const int m_numDataComponents;
     int m_dataComponent;
     bool m_isActive;
+
+    const bool m_mapsScalarsToColors;
 
     /** per data component: value range and user selected subrange */
     std::vector<double> m_dataMinValue;

core/color_mapping/DirectImageColors.cpp
@@ -31,35 +31,39 @@ std::vector<std::unique_ptr<ColorMappingData>> DirectImageColors::newInstances(c
     {
         for (auto i = 0; i < attributes->GetNumberOfArrays(); ++i)
         {
-            vtkUnsignedCharArray * colors = vtkUnsignedCharArray::SafeDownCast(attributes->GetArray(i));
+            auto colors = vtkUnsignedCharArray::SafeDownCast(attributes->GetArray(i));
             if (!colors || colors->GetNumberOfComponents() != 3)
+            {
                 continue;
+            }
 
             // skip arrays that are marked as auxiliary
-            vtkInformation * arrayInfo = colors->GetInformation();
+            auto arrayInfo = colors->GetInformation();
             if (arrayInfo->Has(DataObject::ArrayIsAuxiliaryKey())
                 && arrayInfo->Get(DataObject::ArrayIsAuxiliaryKey()))
+            {
                 continue;
+            }
 
-            QString name = QString::fromUtf8(colors->GetName());
-
-            arrayLocs.insert(name, attributeLocation);
+            arrayLocs.insert(QString::fromUtf8(colors->GetName()), attributeLocation);
         }
     };
 
     QList<AbstractVisualizedData *> supportedData;
 
     // list all available array names, check for same number of components
-    for (AbstractVisualizedData * vis : visualizedData)
+    for (auto vis : visualizedData)
     {
         if (vis->contentType() == ContentType::Context2D)   // don't try to map colors to a plot
+        {
             continue;
+        }
 
         supportedData << vis;
 
         for (auto i = 0; i < vis->numberOfColorMappingInputs(); ++i)
         {
-            vtkDataSet * dataSet = vis->colorMappingInputData(i);
+            auto dataSet = vis->colorMappingInputData(i);
 
             checkAddAttributeArrays(dataSet->GetCellData(), vtkAssignAttribute::CELL_DATA);
             checkAddAttributeArrays(dataSet->GetPointData(), vtkAssignAttribute::POINT_DATA);
@@ -81,7 +85,7 @@ std::vector<std::unique_ptr<ColorMappingData>> DirectImageColors::newInstances(c
 }
 
 DirectImageColors::DirectImageColors(const QList<AbstractVisualizedData *> & visualizedData, QString dataArrayName, int attributeLocation)
-    : ColorMappingData(visualizedData)
+    : ColorMappingData(visualizedData, 1, false)
     , m_attributeLocation{ attributeLocation }
     , m_dataArrayName{ dataArrayName }
 {

core/rendered_data/RenderedImageData.cpp
@@ -24,15 +24,6 @@
 
 using namespace reflectionzeug;
 
-namespace
-{
-    enum Interpolation
-    {
-        nearest = VTK_NEAREST_INTERPOLATION,
-        linear = VTK_LINEAR_INTERPOLATION,
-        cubic = VTK_CUBIC_INTERPOLATION
-    };
-}
 
 RenderedImageData::RenderedImageData(ImageDataObject & dataObject)
     : RenderedData(ContentType::Rendered2D, dataObject)
@@ -131,9 +122,9 @@ void RenderedImageData::scalarsForColorMappingChangedEvent()
 
     if (m_colorMappingData->usesFilter())
     {
-        vtkSmartPointer<vtkAlgorithm> filter = m_colorMappingData->createFilter(this);
+        auto filter = m_colorMappingData->createFilter(this);
         filter->Update();
-        vtkDataSet * image = vtkDataSet::SafeDownCast(filter->GetOutputDataObject(0));
+        auto image = vtkDataSet::SafeDownCast(filter->GetOutputDataObject(0));
         // use filter only if it outputs scalars. To prevent segfault in image slice.
         if (image->GetPointData()->GetScalars())
             m_mapper->SetInputConnection(filter->GetOutputPort());
@@ -148,6 +139,11 @@ void RenderedImageData::scalarsForColorMappingChangedEvent()
     property()->SetOpacity(colorMapping().isEnabled()
         ? 1.0
         : 0.0);
+
+    // hack required for DirectImageColors: how to enforce to use color data directly?
+    property()->SetLookupTable(m_colorMappingData->mapsScalarsToColors()
+        ? m_gradient
+        : nullptr);
 }
 
 void RenderedImageData::colorMappingGradientChangedEvent()

core/rendered_data/RenderedImageData.h
@@ -17,6 +17,13 @@ public:
 
     std::unique_ptr<reflectionzeug::PropertyGroup> createConfigGroup() override;
 
+    enum Interpolation
+    {
+        nearest = VTK_NEAREST_INTERPOLATION,
+        linear = VTK_LINEAR_INTERPOLATION,
+        cubic = VTK_CUBIC_INTERPOLATION
+    };
+
 protected:
     vtkSmartPointer<vtkPropCollection> fetchViewProps() override;
     vtkImageSlice * slice();

tests/gui-test/CMakeLists.txt
@@ -6,6 +6,7 @@ message(STATUS "Test ${target}")
 set(sources
     main.cpp
     app_helper.h
+    ColorMapping_test.cpp
     DEMWidget_test.cpp
     RenderView_test.cpp
     RenderView_test_tools.h

tests/gui-test/ColorMapping_test.cpp
@@ -1,0 +1,139 @@
+#define _SCL_SECURE_NO_WARNINGS
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include <QApplication>
+
+#include <vtkCamera.h>
+#include <vtkImageData.h>
+#include <vtkPointData.h>
+#include <vtkSmartPointer.h>
+#include <vtkUnsignedCharArray.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkVector.h>
+#include <vtkWindowToImageFilter.h>
+
+#include <reflectionzeug/PropertyGroup.h>
+
+#include <core/color_mapping/ColorMapping.h>
+#include <core/data_objects/ImageDataObject.h>
+#include <core/rendered_data/RenderedImageData.h>
+
+#include "app_helper.h"
+
+
+class ColorMapping_test : public testing::Test
+{
+public:
+    QApplication app{ main_argc, main_argv };
+
+    static vtkSmartPointer<vtkUnsignedCharArray> createUCharColors()
+    {
+        auto colorData = std::vector<unsigned char>{ // r, g, b [0,255]; 2x2
+            0, 1, 2,
+            30, 31, 32,
+            180, 181, 182,
+            253, 254, 255
+        };
+
+        auto colorArray = vtkSmartPointer<vtkUnsignedCharArray>::New();
+        colorArray->SetName("Colors");
+        colorArray->SetNumberOfComponents(3);
+        colorArray->SetNumberOfTuples(4);
+        colorArray->SetArray(colorData.data(), static_cast<vtkIdType>(colorData.size()), true);
+
+        return colorArray;
+    }
+
+    struct ImageStruct
+    {
+        std::unique_ptr<ImageDataObject> image;
+        std::unique_ptr<RenderedImageData> rendered;
+        QString scalarsName;
+    };
+
+    static ImageStruct createImage(vtkUnsignedCharArray & colors)
+    {
+        auto imageData = vtkSmartPointer<vtkImageData>::New();
+        imageData->SetExtent(0, 1, 0, 1, 0, 0);
+        imageData->GetPointData()->SetScalars(&colors);
+
+        ImageStruct img;
+
+        img.image = std::make_unique<ImageDataObject>("Picture", *imageData);
+        img.rendered.reset(
+            dynamic_cast<RenderedImageData *>(img.image->createRendered().release()));
+
+        const auto scalars = img.rendered->colorMapping().scalarsNames();
+        const auto directColors = scalars.filter(QRegExp(".*direct colors.*", Qt::CaseInsensitive));
+        img.scalarsName = directColors.isEmpty() ? "" : directColors.first();
+
+        return img;
+    }
+};
+
+
+TEST_F(ColorMapping_test, DirectImageColors_detects_uchar)
+{
+    auto colorArray = createUCharColors();
+    auto img = createImage(*colorArray);
+
+    ASSERT_FALSE(img.scalarsName.isEmpty());
+}
+
+TEST_F(ColorMapping_test, DirectImageColors_renders_uchar)
+{
+    auto colorArray = createUCharColors();
+    auto img = createImage(*colorArray);
+
+    img.rendered->colorMapping().setCurrentScalarsByName(img.scalarsName);
+    auto config = img.rendered->createConfigGroup();
+    auto interpolationProp = config->property<RenderedImageData::Interpolation>("Interpolation");
+    interpolationProp->setValue(RenderedImageData::nearest);
+
+    auto viewProps = img.rendered->viewProps();
+
+    auto ren = vtkSmartPointer<vtkRenderer>::New();
+    for (viewProps->InitTraversal(); auto prop = viewProps->GetNextProp(); )
+    {
+        ren->AddViewProp(prop);
+    }
+
+    auto renWin = vtkSmartPointer<vtkRenderWindow>::New();
+    renWin->AddRenderer(ren);
+    renWin->OffScreenRenderingOn();
+    renWin->SetSize(2, 2);
+    
+    auto &cam = *ren->GetActiveCamera();
+    cam.SetParallelProjection(true);
+    ren->ResetCamera();
+    cam.SetFocalPoint(0.5, 0.5, 0.0);
+    cam.SetParallelScale(0.5);
+
+    auto windowToImage = vtkSmartPointer<vtkWindowToImageFilter>::New();
+    windowToImage->SetInput(renWin);
+    windowToImage->SetInputBufferTypeToRGBA();
+    windowToImage->ReadFrontBufferOff();
+    windowToImage->Update();
+
+    auto renderedImage = windowToImage->GetOutput();
+    auto renderedColors = vtkUnsignedCharArray::SafeDownCast(renderedImage->GetPointData()->GetScalars());
+    assert(renderedColors);
+
+    ASSERT_EQ(colorArray->GetNumberOfTuples(), renderedColors->GetNumberOfTuples());
+
+    for (int i = 0; i < colorArray->GetNumberOfTuples(); ++i)
+    {
+        vtkVector<unsigned char, 4> refRgba;
+        colorArray->GetTypedTuple(i, refRgba.GetData());
+        refRgba[3] = 0xFF;
+
+        vtkVector<unsigned char, 4> testRgba;
+        renderedColors->GetTypedTuple(i, testRgba.GetData());
+
+        ASSERT_EQ(refRgba, testRgba);
+    }
+}