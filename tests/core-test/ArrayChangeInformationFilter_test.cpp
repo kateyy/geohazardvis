kateyy/geohazardvis

commit 9d78e81736f4c0f71991962e1bc341d44e119387

Allow to pass though the input array in ArrayChangeInformationFilter

core/filters/ArrayChangeInformationFilter.cpp
@@ -55,6 +55,7 @@ ArrayChangeInformationFilter::ArrayChangeInformationFilter()
     , ArrayName{ nullptr }
     , EnableSetUnit{ false }
     , ArrayUnit{ nullptr }
+    , PassInputArray{ false }
 {
 }
 
@@ -62,6 +63,51 @@ ArrayChangeInformationFilter::~ArrayChangeInformationFilter()
 {
     this->SetArrayName(nullptr);
     this->SetArrayUnit(nullptr);
+}
+
+int ArrayChangeInformationFilter::RequestInformation(
+    vtkInformation * request,
+    vtkInformationVector ** inputVector,
+    vtkInformationVector * outputVector)
+{
+    if (!Superclass::RequestInformation(request, inputVector, outputVector))
+    {
+        return 0;
+    }
+
+    const int fieldAssociation = this->AttributeLocation == POINT_DATA
+        ? vtkDataObject::FIELD_ASSOCIATION_POINTS
+        : vtkDataObject::FIELD_ASSOCIATION_CELLS;
+
+    auto inInfo = inputVector[0]->GetInformationObject(0);
+
+    vtkIdType arrayType = -1;
+    vtkIdType numComponents = -1;
+    vtkIdType numTuples = -1;
+    const char * inName = nullptr;
+
+    if (auto inFieldInfo = vtkDataObject::GetActiveFieldInformation(
+        inInfo,
+        fieldAssociation,
+        this->AttributeType))
+    {
+        arrayType = inFieldInfo->Get(vtkDataObject::FIELD_ARRAY_TYPE());
+        numComponents = inFieldInfo->Get(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS());
+        numTuples = inFieldInfo->Get(vtkDataObject::FIELD_NUMBER_OF_TUPLES());
+        inName = inFieldInfo->Get(vtkDataObject::FIELD_NAME());
+    }
+
+    auto outInfo = outputVector->GetInformationObject(0);
+    vtkDataObject::SetActiveAttributeInfo(
+        outInfo,
+        fieldAssociation,
+        this->AttributeType,
+        this->EnableRename ? this->ArrayName : inName,
+        arrayType != 0 ? arrayType : -1,
+        numComponents > 0 ? numComponents : -1,
+        numTuples > 0 ? numTuples : -1);
+
+    return 1;
 }
 
 int ArrayChangeInformationFilter::RequestData(
@@ -107,28 +153,32 @@ int ArrayChangeInformationFilter::RequestData(
         outAttribute.TakeReference(inAttribute->NewInstance());
         outAttribute->DeepCopy(inAttribute);
 
-        int outAttrIndexPassed = -1;
-        for (int i = 0; i < inDsAttributes->GetNumberOfArrays(); ++i)
+        if (this->EnableRename)
         {
-            if (inDsAttributes->GetArray(i) == inAttribute)
-            {
-                outAttrIndexPassed = i;
-                break;
-            }
+            outAttribute->SetName(this->ArrayName);
         }
 
-        assert(outAttrIndexPassed != -1);
-
         // replace the array (and active attribute) by a copy
-        outDsAttributes->RemoveArray(outAttrIndexPassed);
+        if (!this->PassInputArray || !this->EnableRename)
+        {
+            int outAttrIndexPassed = -1;
+            for (int i = 0; i < inDsAttributes->GetNumberOfArrays(); ++i)
+            {
+                if (inDsAttributes->GetArray(i) == inAttribute)
+                {
+                    outAttrIndexPassed = i;
+                    break;
+                }
+            }
+
+            assert(outAttrIndexPassed != -1);
+
+            outDsAttributes->RemoveArray(outAttrIndexPassed);
+        }
         const int outAttrIndexCopied = outDsAttributes->AddArray(outAttribute);
         outDsAttributes->SetActiveAttribute(outAttrIndexCopied, this->AttributeType);
     }
 
-    if (this->EnableRename)
-    {
-        outAttribute->SetName(this->ArrayName);
-    }
 
 #if VTK_CHECK_VERSION(7, 1, 0)
     if (this->EnableSetUnit)

core/filters/ArrayChangeInformationFilter.h
@@ -47,9 +47,22 @@ public:
     /** Set the vtkDataArray::UNITS_LABEL on the array. This requires VTK version 7.1.0 or newer */
     vtkSetStringMacro(ArrayUnit);
 
+
+    /** Also pass the input array to the output.
+    * By default, the input array is copied (and information are changed), but not passed to the output.
+    * This does only make sense if renaming is enabled, since data set attributes to not meant to 
+    * be used with multiple arrays with same names. */
+    vtkBooleanMacro(PassInputArray, bool);
+    vtkGetMacro(PassInputArray, bool);
+    vtkSetMacro(PassInputArray, bool);
+
 protected:
     ArrayChangeInformationFilter();
     ~ArrayChangeInformationFilter() override;
+
+    int RequestInformation(vtkInformation * request,
+        vtkInformationVector ** inputVector,
+        vtkInformationVector * outputVector) override;
 
     int RequestData(vtkInformation * request,
         vtkInformationVector ** inputVector,
@@ -68,4 +81,6 @@ private:
 
     bool EnableSetUnit;
     char * ArrayUnit;
+
+    bool PassInputArray;
 };

commit 9c9037d472e434b4aeccd5984598cf6a3be2d9c5

Test ArrayChangeInformationFilter/vtkAssignAttribute behavior
* some these tests will only work with patched vtkAssignAttribute (VTK 7.1+)
* patched vtkAssignAttribute fixes a regression indirectly introduced by 9d78e81736f4c0f71991962e1bc341d44e119387 (this repo)
    - number of components passed in data and info path were not consistent, causing segmentation faults in vtkImageNormalize
    - this cased LIC2D rendering to crash

tests/core-test/ArrayChangeInformationFilter_test.cpp
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
 
+#include <vtkAssignAttribute.h>
 #include <vtkCellData.h>
 #include <vtkFloatArray.h>
+#include <vtkImageData.h>
+#include <vtkImageNormalize.h>
 #include <vtkInformation.h>
+#include <vtkInformationVector.h>
 #include <vtkPointData.h>
 #include <vtkPolyData.h>
 #include <vtkSmartPointer.h>
@@ -10,6 +14,8 @@
 
 #include <core/filters/ArrayChangeInformationFilter.h>
 #include <core/utility/macros.h>
+
+#include "PipelineInformationHelper.h"
 
 
 class ArrayChangeInformationFilter_test : public testing::Test
@@ -115,4 +121,307 @@ TEST_F(ArrayChangeInformationFilter_test, AttributeLocationAndType)
 
     ASSERT_TRUE(outNormals);
     ASSERT_STREQ("outNormals", outNormals->GetName());
+}
+
+TEST_F(ArrayChangeInformationFilter_test, KeepActiveVectors)
+{
+    inAttr->SetNumberOfComponents(3);
+    inAttr->SetName("inVectors");
+    inDs->GetPointData()->SetVectors(inAttr);
+
+    filter->SetAttributeLocation(ArrayChangeInformationFilter::POINT_DATA);
+    filter->SetAttributeType(vtkDataSetAttributes::VECTORS);
+    filter->SetArrayName("outVectors");
+    filter->Update();
+
+    auto outVectors = filter->GetOutput()->GetPointData()->GetVectors();
+
+    ASSERT_TRUE(outVectors);
+    ASSERT_STREQ("outVectors", outVectors->GetName());
+}
+
+TEST_F(ArrayChangeInformationFilter_test, PassScalarInformation)
+{
+    inAttr->SetNumberOfComponents(5);
+    inAttr->SetNumberOfTuples(17);
+    auto inImage = vtkSmartPointer<vtkImageData>::New();
+    inImage->GetPointData()->SetScalars(inAttr);
+    inImage->SetExtent(0, 0, 2, 18, 3, 3);
+
+    auto infoSource = vtkSmartPointer<InformationSource>::New();
+    infoSource->SetOutput(inImage);
+
+    vtkDataObject::SetActiveAttributeInfo(infoSource->GetOutInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::SCALARS,
+        inAttr->GetName(),
+        inAttr->GetDataType(),
+        inAttr->GetNumberOfComponents(),
+        inAttr->GetNumberOfTuples());
+
+    filter->SetAttributeLocation(ArrayChangeInformationFilter::POINT_DATA);
+    filter->SetAttributeType(vtkDataSetAttributes::SCALARS);
+    filter->SetInputConnection(infoSource->GetOutputPort());
+    filter->EnableRenameOff();
+    filter->EnableSetUnitOff();
+
+    auto getInfo = vtkSmartPointer<InformationSink>::New();
+    getInfo->SetInputConnection(filter->GetOutputPort());
+
+    getInfo->UpdateInformation();
+
+    auto outInfo = vtkDataObject::GetActiveFieldInformation(getInfo->GetInInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::SCALARS);
+
+    ASSERT_TRUE(outInfo);
+
+    ASSERT_TRUE(outInfo->Has(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS()));
+    ASSERT_EQ(inAttr->GetNumberOfComponents(), outInfo->Get(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS()));
+
+    ASSERT_TRUE(outInfo->Has(vtkDataObject::FIELD_NUMBER_OF_TUPLES()));
+    ASSERT_EQ(inAttr->GetNumberOfTuples(), outInfo->Get(vtkDataObject::FIELD_NUMBER_OF_TUPLES()));
+
+    ASSERT_TRUE(outInfo->Has(vtkDataObject::FIELD_NAME()));
+    ASSERT_STREQ(inAttr->GetName(), outInfo->Get(vtkDataObject::FIELD_NAME()));
+
+    ASSERT_TRUE(outInfo->Has(vtkDataObject::FIELD_ARRAY_TYPE()));
+    ASSERT_EQ(inAttr->GetDataType(), outInfo->Get(vtkDataObject::FIELD_ARRAY_TYPE()));
+}
+
+TEST_F(ArrayChangeInformationFilter_test, PassVectorInformation)
+{
+    inAttr->SetNumberOfComponents(3);
+    inAttr->SetNumberOfTuples(17);
+    auto inImage = vtkSmartPointer<vtkImageData>::New();
+    inImage->GetPointData()->SetVectors(inAttr);
+    inImage->SetExtent(0, 0, 2, 18, 3, 3);
+
+    auto infoSource = vtkSmartPointer<InformationSource>::New();
+    infoSource->SetOutput(inImage);
+
+    vtkDataObject::SetActiveAttributeInfo(infoSource->GetOutInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::VECTORS,
+        inAttr->GetName(),
+        inAttr->GetDataType(),
+        inAttr->GetNumberOfComponents(),
+        inAttr->GetNumberOfTuples());
+
+    filter->SetAttributeLocation(ArrayChangeInformationFilter::POINT_DATA);
+    filter->SetAttributeType(vtkDataSetAttributes::VECTORS);
+    filter->SetInputConnection(infoSource->GetOutputPort());
+    filter->EnableRenameOff();
+    filter->EnableSetUnitOff();
+
+    auto getInfo = vtkSmartPointer<InformationSink>::New();
+    getInfo->SetInputConnection(filter->GetOutputPort());
+
+    getInfo->UpdateInformation();
+
+    auto outInfo = vtkDataObject::GetActiveFieldInformation(getInfo->GetInInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::VECTORS);
+
+    ASSERT_TRUE(outInfo);
+
+    ASSERT_TRUE(outInfo->Has(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS()));
+    ASSERT_EQ(inAttr->GetNumberOfComponents(), outInfo->Get(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS()));
+
+    ASSERT_TRUE(outInfo->Has(vtkDataObject::FIELD_NUMBER_OF_TUPLES()));
+    ASSERT_EQ(inAttr->GetNumberOfTuples(), outInfo->Get(vtkDataObject::FIELD_NUMBER_OF_TUPLES()));
+
+    ASSERT_TRUE(outInfo->Has(vtkDataObject::FIELD_NAME()));
+    ASSERT_STREQ(inAttr->GetName(), outInfo->Get(vtkDataObject::FIELD_NAME()));
+
+    ASSERT_TRUE(outInfo->Has(vtkDataObject::FIELD_ARRAY_TYPE()));
+    ASSERT_EQ(inAttr->GetDataType(), outInfo->Get(vtkDataObject::FIELD_ARRAY_TYPE()));
+}
+
+TEST_F(ArrayChangeInformationFilter_test, CheckWhichInformation_vtkAssignAttribute_passes)
+{
+    inAttr->SetNumberOfComponents(3);
+    inAttr->SetNumberOfTuples(17);
+    auto inImage = vtkSmartPointer<vtkImageData>::New();
+    inImage->GetPointData()->SetScalars(inAttr);
+    inImage->SetExtent(0, 0, 2, 18, 3, 3);
+
+    auto infoSource = vtkSmartPointer<InformationSource>::New();
+    infoSource->SetOutput(inImage);
+
+    vtkDataObject::SetActiveAttributeInfo(infoSource->GetOutInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::SCALARS,
+        inAttr->GetName(),
+        inAttr->GetDataType(),
+        inAttr->GetNumberOfComponents(),
+        inAttr->GetNumberOfTuples());
+
+    filter->SetAttributeLocation(ArrayChangeInformationFilter::POINT_DATA);
+    filter->SetAttributeType(vtkDataSetAttributes::SCALARS);
+    filter->SetInputConnection(infoSource->GetOutputPort());
+    filter->EnableRenameOff();
+    filter->EnableSetUnitOff();
+
+    auto assignToVectors = vtkSmartPointer<vtkAssignAttribute>::New();
+    assignToVectors->SetInputConnection(filter->GetOutputPort());
+    assignToVectors->Assign(vtkDataSetAttributes::SCALARS, vtkDataSetAttributes::VECTORS,
+        vtkAssignAttribute::POINT_DATA);
+
+    auto getInfo = vtkSmartPointer<InformationSink>::New();
+    getInfo->SetInputConnection(assignToVectors->GetOutputPort());
+
+    getInfo->UpdateInformation();
+
+    auto outScalarInfo = vtkDataObject::GetActiveFieldInformation(getInfo->GetInInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::SCALARS);
+
+    ASSERT_TRUE(outScalarInfo);
+
+    ASSERT_TRUE(outScalarInfo->Has(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS()));
+    ASSERT_EQ(inAttr->GetNumberOfComponents(), outScalarInfo->Get(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS()));
+
+    ASSERT_FALSE(outScalarInfo->Has(vtkDataObject::FIELD_NUMBER_OF_TUPLES()));
+
+    ASSERT_FALSE(outScalarInfo->Has(vtkDataObject::FIELD_NAME()));
+
+    ASSERT_TRUE(outScalarInfo->Has(vtkDataObject::FIELD_ARRAY_TYPE()));
+    ASSERT_EQ(inAttr->GetDataType(), outScalarInfo->Get(vtkDataObject::FIELD_ARRAY_TYPE()));
+
+    auto outVectorInfo = vtkDataObject::GetActiveFieldInformation(getInfo->GetInInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::VECTORS);
+
+    ASSERT_TRUE(outVectorInfo);
+
+    ASSERT_TRUE(outVectorInfo->Has(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS()));
+    ASSERT_EQ(inAttr->GetNumberOfComponents(), outVectorInfo->Get(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS()));
+
+    ASSERT_TRUE(outVectorInfo->Has(vtkDataObject::FIELD_NUMBER_OF_TUPLES()));
+    ASSERT_EQ(inAttr->GetNumberOfTuples(), outVectorInfo->Get(vtkDataObject::FIELD_NUMBER_OF_TUPLES()));
+
+    ASSERT_TRUE(outVectorInfo->Has(vtkDataObject::FIELD_NAME()));
+    ASSERT_STREQ(inAttr->GetName(), outVectorInfo->Get(vtkDataObject::FIELD_NAME()));
+
+    ASSERT_TRUE(outVectorInfo->Has(vtkDataObject::FIELD_ARRAY_TYPE()));
+    ASSERT_EQ(inAttr->GetDataType(), outVectorInfo->Get(vtkDataObject::FIELD_ARRAY_TYPE()));
+}
+
+TEST_F(ArrayChangeInformationFilter_test, CheckWhichInformation_vtkAssignAttribute_x2_passes)
+{
+    inAttr->SetNumberOfComponents(3);
+    inAttr->SetNumberOfTuples(17);
+    auto inImage = vtkSmartPointer<vtkImageData>::New();
+    inImage->GetPointData()->SetScalars(inAttr);
+    inImage->SetExtent(0, 0, 2, 18, 3, 3);
+
+    auto infoSource = vtkSmartPointer<InformationSource>::New();
+    infoSource->SetOutput(inImage);
+
+    vtkDataObject::SetActiveAttributeInfo(infoSource->GetOutInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::SCALARS,
+        inAttr->GetName(),
+        inAttr->GetDataType(),
+        inAttr->GetNumberOfComponents(),
+        inAttr->GetNumberOfTuples());
+
+    filter->SetAttributeLocation(ArrayChangeInformationFilter::POINT_DATA);
+    filter->SetAttributeType(vtkDataSetAttributes::SCALARS);
+    filter->SetInputConnection(infoSource->GetOutputPort());
+    filter->EnableRenameOff();
+    filter->EnableSetUnitOff();
+
+    auto assignToVectors = vtkSmartPointer<vtkAssignAttribute>::New();
+    assignToVectors->SetInputConnection(filter->GetOutputPort());
+    assignToVectors->Assign(vtkDataSetAttributes::SCALARS, vtkDataSetAttributes::VECTORS,
+        vtkAssignAttribute::POINT_DATA);
+
+    auto reassignToScalars = vtkSmartPointer<vtkAssignAttribute>::New();
+    reassignToScalars->SetInputConnection(assignToVectors->GetOutputPort());
+    reassignToScalars->Assign(vtkDataSetAttributes::VECTORS, vtkDataSetAttributes::SCALARS,
+        vtkAssignAttribute::POINT_DATA);
+
+    auto getInfo = vtkSmartPointer<InformationSink>::New();
+    getInfo->SetInputConnection(reassignToScalars->GetOutputPort());
+
+    getInfo->UpdateInformation();
+
+    auto outScalarInfo = vtkDataObject::GetActiveFieldInformation(getInfo->GetInInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::SCALARS);
+
+    ASSERT_TRUE(outScalarInfo);
+
+    ASSERT_TRUE(outScalarInfo->Has(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS()));
+    ASSERT_EQ(inAttr->GetNumberOfComponents(), outScalarInfo->Get(vtkDataObject::FIELD_NUMBER_OF_COMPONENTS()));
+
+    ASSERT_TRUE(outScalarInfo->Has(vtkDataObject::FIELD_NUMBER_OF_TUPLES()));
+    ASSERT_EQ(inAttr->GetNumberOfTuples(), outScalarInfo->Get(vtkDataObject::FIELD_NUMBER_OF_TUPLES()));
+
+    ASSERT_TRUE(outScalarInfo->Has(vtkDataObject::FIELD_NAME()));
+    ASSERT_STREQ(inAttr->GetName(), outScalarInfo->Get(vtkDataObject::FIELD_NAME()));
+
+    ASSERT_TRUE(outScalarInfo->Has(vtkDataObject::FIELD_ARRAY_TYPE()));
+    ASSERT_EQ(inAttr->GetDataType(), outScalarInfo->Get(vtkDataObject::FIELD_ARRAY_TYPE()));
+
+    auto outVectorInfo = vtkDataObject::GetActiveFieldInformation(getInfo->GetInInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::VECTORS);
+
+    ASSERT_FALSE(outVectorInfo);
+}
+
+TEST_F(ArrayChangeInformationFilter_test, vtkAssignAttribute_CorrectNumberOfComponentsPassedDownstream)
+{
+    inAttr->SetNumberOfComponents(3);
+    inAttr->SetNumberOfTuples(17);
+    for (vtkIdType i = 0; i < inAttr->GetNumberOfValues(); ++i)
+    {
+        inAttr->SetValue(i, static_cast<float>(i));
+    }
+    auto inImage = vtkSmartPointer<vtkImageData>::New();
+    inImage->GetPointData()->SetScalars(inAttr);
+    inImage->SetExtent(0, 0, 2, 18, 3, 3);
+
+    auto infoSource = vtkSmartPointer<InformationSource>::New();
+    infoSource->SetOutput(inImage);
+
+    vtkDataObject::SetActiveAttributeInfo(infoSource->GetOutInfo(),
+        vtkDataObject::FIELD_ASSOCIATION_POINTS,
+        vtkDataSetAttributes::SCALARS,
+        inAttr->GetName(),
+        inAttr->GetDataType(),
+        inAttr->GetNumberOfComponents(),
+        inAttr->GetNumberOfTuples());
+
+    filter->SetAttributeLocation(ArrayChangeInformationFilter::POINT_DATA);
+    filter->SetAttributeType(vtkDataSetAttributes::SCALARS);
+    filter->SetInputConnection(infoSource->GetOutputPort());
+    filter->EnableRenameOff();
+    filter->EnableSetUnitOff();
+
+    auto assignToVectors = vtkSmartPointer<vtkAssignAttribute>::New();
+    assignToVectors->SetInputConnection(filter->GetOutputPort());
+    assignToVectors->Assign(vtkDataSetAttributes::SCALARS, vtkDataSetAttributes::VECTORS,
+        vtkAssignAttribute::POINT_DATA);
+
+    auto reassignScalars = vtkSmartPointer<vtkAssignAttribute>::New();
+    reassignScalars->SetInputConnection(assignToVectors->GetOutputPort());
+    reassignScalars->Assign(vtkDataSetAttributes::VECTORS, vtkDataSetAttributes::SCALARS,
+        vtkAssignAttribute::POINT_DATA);
+
+    auto normalize = vtkSmartPointer<vtkImageNormalize>::New();
+    normalize->SetInputConnection(reassignScalars->GetOutputPort());
+    normalize->SetEnableSMP(false);
+    normalize->SetNumberOfThreads(1);
+
+    // before patching (VTK 7.1+) vtkAssignAttribute, this would cause segmentation faults
+    normalize->Update();
+    auto outImg = normalize->GetOutput();
+    auto outScalars = outImg->GetPointData()->GetScalars();
+
+    ASSERT_EQ(inAttr->GetNumberOfComponents(), outScalars->GetNumberOfComponents());
+    ASSERT_EQ(inAttr->GetNumberOfTuples(), outScalars->GetNumberOfTuples());
 }

tests/core-test/CMakeLists.txt
@@ -12,6 +12,8 @@ set(sources
     DataSetFilter_test.cpp
     DataSetHandler_test.cpp
     DEMToTopographyMesh_test.cpp
+    PipelineInformationHelper.h
+    PipelineInformationHelper.cpp
     RenderedData_test.cpp
     TextFileReader_test.cpp
     VTKAlgorithmImplementations_test.cpp

tests/core-test/PipelineInformationHelper.cpp
@@ -1,0 +1,110 @@
+#include "PipelineInformationHelper.h"
+
+#include <vtkInformation.h>
+#include <vtkInformationVector.h>
+#include <vtkDemandDrivenPipeline.h>
+#include <vtkObjectFactory.h>
+#include <vtkSmartPointer.h>
+
+
+vtkStandardNewMacro(InformationSource);
+
+
+void InformationSource::SetOutInfo(vtkInformation * info)
+{
+    this->OutInfo = info;
+    this->Modified();
+}
+
+vtkInformation * InformationSource::GetOutInfo()
+{
+    if (!this->OutInfo)
+    {
+        this->OutInfo = vtkSmartPointer<vtkInformation>::New();
+    }
+
+    return this->OutInfo;
+}
+
+InformationSource::InformationSource()
+    : Superclass()
+{
+}
+
+InformationSource::~InformationSource() = default;
+
+int InformationSource::ProcessRequest(vtkInformation * request,
+    vtkInformationVector ** inVector, vtkInformationVector * outVector)
+{
+    if (!this->Superclass::ProcessRequest(request, inVector, outVector))
+    {
+        return 0;
+    }
+
+    if (request->Has(vtkDemandDrivenPipeline::REQUEST_INFORMATION()))
+    {
+        return RequestInformation(request, inVector, outVector);
+    }
+
+    return 1;
+}
+
+int InformationSource::RequestInformation(vtkInformation * /*request*/,
+    vtkInformationVector ** /*inVector*/, vtkInformationVector * outVector)
+{
+    if (!this->OutInfo)
+    {
+        return 1;
+    }
+
+    auto outInfo = outVector->GetInformationObject(0);
+
+    outInfo->Append(this->OutInfo, 1);
+
+    return 1;
+}
+
+
+vtkStandardNewMacro(InformationSink);
+
+
+vtkInformation * InformationSink::GetInInfo()
+{
+    return this->InInfo;
+}
+
+InformationSink::InformationSink()
+    : Superclass()
+    , InInfo{ vtkSmartPointer<vtkInformation>::New() }
+{
+    this->SetNumberOfInputPorts(1);
+    this->SetNumberOfOutputPorts(0);
+}
+
+InformationSink::~InformationSink() = default;
+
+int InformationSink::FillInputPortInformation(int /*port*/, vtkInformation * /*info*/)
+{
+    return 1;
+}
+
+int InformationSink::ProcessRequest(vtkInformation * request,
+    vtkInformationVector ** inVector, vtkInformationVector * outVector)
+{
+    if (request->Has(vtkDemandDrivenPipeline::REQUEST_INFORMATION()))
+    {
+        return RequestInformation(request, inVector, outVector);
+    }
+
+    return Superclass::ProcessRequest(request, inVector, outVector);
+}
+
+int InformationSink::RequestInformation(vtkInformation * /*request*/,
+    vtkInformationVector ** inVector, vtkInformationVector * /*outVector*/)
+{
+    auto inInfo = inVector[0]->GetInformationObject(0);
+
+    this->InInfo->Copy(inInfo, 1);
+
+    return 1;
+}

tests/core-test/PipelineInformationHelper.h
@@ -1,0 +1,67 @@
+#pragma once
+
+#include <vtkSmartPointer.h>
+#include <vtkTrivialProducer.h>
+
+
+class InformationSource : public vtkTrivialProducer
+{
+public:
+    vtkTypeMacro(InformationSource, vtkTrivialProducer);
+
+    static InformationSource * New();
+
+    void SetOutInfo(vtkInformation * info);
+    vtkInformation * GetOutInfo();
+
+protected:
+    InformationSource();
+    ~InformationSource() override;
+
+    int ProcessRequest(vtkInformation * request,
+        vtkInformationVector ** inVector,
+        vtkInformationVector * outVector) override;
+
+    int RequestInformation(vtkInformation * request,
+        vtkInformationVector ** inVector,
+        vtkInformationVector * outVector);
+
+private:
+    vtkSmartPointer<vtkInformation> OutInfo;
+
+private:
+    InformationSource(const InformationSource &) = delete;
+    void operator=(const InformationSource &) = delete;
+};
+
+
+class InformationSink : public vtkAlgorithm
+{
+public:
+    vtkTypeMacro(InformationSink, vtkAlgorithm);
+
+    static InformationSink * New();
+
+    vtkInformation * GetInInfo();
+
+protected:
+    InformationSink();
+    ~InformationSink() override;
+
+    int FillInputPortInformation(int port, vtkInformation * info) override;
+
+    int ProcessRequest(vtkInformation * request,
+        vtkInformationVector ** inVector,
+        vtkInformationVector * outVector) override;
+
+    int RequestInformation(vtkInformation * request,
+        vtkInformationVector ** inVector,
+        vtkInformationVector * outVector);
+
+private:
+    vtkSmartPointer<vtkInformation> InInfo;
+
+private:
+    InformationSink(const InformationSink &) = delete;
+    void operator=(const InformationSink &) = delete;
+};