67db3d96f1958ee79aa71d4203f1754f5c9ec81c
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2017-05-08 11:36:26 +0200

    Try to use system default font in VTK on Windows

--- core/utility/font.cpp
+++ core/utility/font.cpp
@@ -3,7 +3,9 @@
 #include <vtkTextProperty.h>
 
 #if WIN32
+#include <QFont>
 #include <QStandardPaths>
+#include <QSettings>
 #elif __linux__
 #include <vtkFreeTypeTools.h>
 #endif
@@ -21,9 +23,53 @@ bool isUnicodeFontAvailable()
 const char * unicodeFontFileName()
 {
 #if WIN32
-    static const auto fileName =
-        QStandardPaths::locate(QStandardPaths::FontsLocation, "arial.ttf").toUtf8();
-    return fileName.constData();
+    static const auto filePath = [] () -> QString
+    {
+        auto fileName = [] () -> QString
+        {
+            const QSettings fontSettings(
+                "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Fonts",
+                QSettings::NativeFormat);
+            const auto defaultFont = QFont().family();
+            const auto fontPath = fontSettings.value(defaultFont).toString();
+            if (!fontPath.isEmpty())
+            {
+                return fontPath;
+            }
+            const QSettings substitutesSettings(
+                "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows NT\\CurrentVersion\\FontSubstitutes",
+                QSettings::NativeFormat);
+            auto substitute = substitutesSettings.value(defaultFont).toString();
+            if (substitute.isEmpty())
+            {
+                return{};
+            }
+            auto substitutePath = fontSettings.value(substitute).toString();
+            if (!substitutePath.isEmpty())
+            {
+                return substitutePath;
+            }
+            substitutePath = fontSettings.value(substitute + " (TrueType)").toString();
+            if (!substitutePath.isEmpty())
+            {
+                return substitutePath;
+            }
+            return{};
+        }();
+
+        // Fall-back to a font that should exist on all windows machines
+        if (fileName.isEmpty())
+        {
+            fileName = "arial.ttf";
+        }
+        auto location = QStandardPaths::locate(QStandardPaths::FontsLocation, fileName);
+        if (!location.isEmpty())
+        {
+            return location;
+        }
+        return{};
+    }().toUtf8();
+    return filePath.isEmpty() ? nullptr : filePath.constData();
 
 #elif __linux__
     // On Linux this might be handled within VTK by FreeType/FontConfig
