b4c8ae5ffc704cfd58b0ff592ed2eb86ba7f050b
Karsten Tausche <geodev@posteo.org>
2017-07-19 16:47:59 +0200

    Use system default font and size in AboutProjectDialog

--- gui/AboutProjectDialog.cpp
+++ gui/AboutProjectDialog.cpp
@@ -32,7 +32,7 @@ AboutProjectDialog::AboutProjectDialog(QWidget * parent, Qt::WindowFlags f)
     const QString header = R"(<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
 <html><head><meta name="qrichtext" content="1" /><style type="text/css">
 p, li { white-space: pre-wrap; }
-</style></head><body style=" font-family:'Cantarell'; font-size:11pt; font-weight:400; font-style:normal;">
+</style></head><body style="font-weight:400; font-style:normal;">
 <p align="center" style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-weight:600;">)";
     const QString preVersion = R"(</span></p>
 <p align="center" style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">)";
@@ -96,8 +96,14 @@ p, li { white-space: pre-wrap; }
         thirdPartyString += " - " + thirdParty + "\n";
     }
     thirdPartyString += "\n";
-    thirdPartyString += QString(R"(Plese refer to the ")") + config::installThirdPartyLicensePath +
-        R"(" folder in the appplication folder for further information and licenses.)";
+    thirdPartyString += QString(R"(Please refer to the ")") + config::installThirdPartyLicensePath +
+        R"(" folder in the application folder for further information and licenses.)";
+
+    m_ui->licenseEdit->setText(R"(This program is free software : you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.If not, see <http://www.gnu.org/licenses/>.))");
 
     m_ui->thirdPartyEdit->setText(thirdPartyString);
 }
