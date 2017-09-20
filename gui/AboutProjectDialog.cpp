c0a62ecaac4da32dfa54d43c5c1fe959f776fffb
Karsten Tausche <geodev@posteo.org>
2017-09-20 14:16:54 +0200

    Fix AboutProjectDialog/license formating

--- gui/AboutProjectDialog.cpp
+++ gui/AboutProjectDialog.cpp
@@ -99,12 +99,6 @@ p, li { white-space: pre-wrap; }
     thirdPartyString += QString(R"(Please refer to the ")") + config::installThirdPartyLicensePath +
         R"(" folder in the application folder for further information and licenses.)";
 
-    m_ui->licenseEdit->setText(R"(This program is free software : you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program.If not, see <http://www.gnu.org/licenses/>.))");
-
     m_ui->thirdPartyEdit->setText(thirdPartyString);
 }
 
